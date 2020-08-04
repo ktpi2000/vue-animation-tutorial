840792c928314012ecb4de04b07d7723ee27a0bb
ktpi2000 <katapi2000@gmail.com>
2020-08-04 16:48:28 +0900

    animationのデモページ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,67 @@
 <template>
-  <h2>hello</h2>
+  <div id="helloworld">
+    <div class="section1">
+      <div class="fadeIn">
+        テステステスト
+      </div>
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+    </div>
+    <div class="scroll">scroll down</div>
+    <div class="section2">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+    </div>
+    <div class="scroll">scroll down</div>
+    <div class="section3">
+      <div class="fadeIn">
+        テステステスト
+      </div>
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+        <img src="@/assets/tokyo.png" width="200px" v-inview:class="{'fadeIn': true}" class="image">
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.scroll {
+  height: 2000px;
+}
 
+.image {
+  float: left;
+}
+
+.fadeIn {
+  /* 省略できる*/
+  animation-name: fadein;
+  animation-duration: 3s;
+  animation-timing-function: ease;
+  animation-delay: 0s;
+  animation-iteration-count: 1;
+  animation-direction: none;
+  animation-fill-mode: forwards;
+
+  /*
+  animation: 0.3s fade $easeInOut 0s 1 normal forwards;
+  */
+}
+
+@keyframes fadein {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
